02f9f5e32eccf5a959dfcc7551f8888cd24c5643
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-10-22 13:39:35 -0300

    Se finaliza el dashboard

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,20 +1,34 @@
-// app/sign-in/page.tsx
 "use client";
 import { SignIn as StackSignIn } from "@stackframe/stack";
 import Link from "next/link";
 import { IoArrowBackSharp } from "react-icons/io5";
+import { MdDashboard } from "react-icons/md";
 
 export default function SignIn() {
     return (
         <>
-            <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-md space-y-8 stack-signin">
+            <div className="min-h-screen flex items-center justify-center py-5 px-4 sm:px-6 lg:px-8 text-center">
+                <div className="w-full max-w-md space-y-5 stack-signin text-center">
                     <StackSignIn />
-                    <br />
-                    <Link href="/" className="text-white font-bold py-2 px-4 flex items-center">
+
+                    <Link
+                        href="/dashboard"
+                        className="text-white font-bold py-7 px-4 rounded-none transition duration-300 ease-in-out flex items-center"
+                    >
+                        <MdDashboard className="mr-2" />
+                        Go Dashboard
+                    </Link>
+
+                    {/**
+                     * 
+                     *                     <Link href="/" className="text-white font-bold py-2 px-4 flex items-center">
                         <IoArrowBackSharp className="mr-2 " />
                         Back Home
                     </Link>
+                     * 
+                     */}
+
+
                 </div>
             </div>
         </>
